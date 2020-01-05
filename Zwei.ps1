2765326c35ddb631168980c3e86233595ee37d80
TestMadman <59333688+TestMadman@users.noreply.github.com>
2020-01-05 01:41:05 +0100

    Update Zwei.ps1

--- Zwei.ps1
+++ Zwei.ps1
@@ -4,7 +4,7 @@ $To = "madman1237890026525@gmail.com"
 $Pass = "Madman009"
 $Subject = "BRO!"
 $Body = "WHAT UP MR.POWERSCRIPTS?"
-$Creds = (Get-Credential -Credential "$MyEmail" "$Pass")
+$Creds = new-object Management.Automation.PSCredential $MyEmail, ($Pass | ConvertTo-SecureString -AsPlainText -Force)
 Start-Sleep 2
 
 Send-MailMessage -To $to -From $MyEmail -Subject $Subject -Body $Body -SmtpServer $SMTP -Credential $Creds -UseSsl -Port 587 -DeliveryNotificationOption never
